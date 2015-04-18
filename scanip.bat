8913c5037b3916a060af97d6cdc4812bb5cc0bf0
Denis Akimov <pnmice@gmail.com>
2015-04-17 23:01:24 -0700

    Rename

---
+++ scanip.bat
@@ -0,0 +1,18 @@
+
+@echo off
+
+SET count=0
+SET subnet=192.168.1
+
+:start
+SET /a count=%count%+1
+
+
+ping -n 1 -w 1000 %subnet%.%count% > nul
+
+IF %errorlevel%==0 echo %subnet%.%count% UP  
+IF %errorlevel%==1 echo %subnet%.%count% DOWN 
+
+IF %count%==254 goto :eof
+
+GOTO start
