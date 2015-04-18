85b35509b6f87bfb39e39ba929835a961ea34b8a
pnmice <pnmice@gmail.com>
2015-04-17 23:09:38 -0700

    Update scanip.bat

--- scanip.bat
+++ scanip.bat
@@ -10,7 +10,7 @@ SET /a count=%count%+1
 
 ping -n 1 -w 1000 %subnet%.%count% > nul
 
-IF %errorlevel%==0 echo %subnet%.%count% UP  
+IF %errorlevel%==0 echo %subnet%.%count% >> ip.log
 IF %errorlevel%==1 echo %subnet%.%count% DOWN 
 
 IF %count%==254 goto :eof
